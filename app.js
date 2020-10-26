0f29bdc1705bb6556faed61f330a8daba39839cd
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-10-26 16:36:03 -0700

    Setting up files

---
+++ app.js
@@ -0,0 +1,1 @@
+const apiKey = 'P5eVJCYm13b2xZpxdDtcOMJjzAYLtY4B';
---
+++ app.test.js
@@ -0,0 +1,29 @@
+define('getGif()', () => {
+    
+});
+
+describe('appendGif()', () => {
+
+    it('should append a gif to the page', () => {
+
+    });
+
+    it('should handle multiple gifs appended to the page', () => {
+
+    });
+});
+    
+describe('removeGifs()', () => {
+    it('should remove a single gif from a page if only one exists', () => {
+
+    });
+
+    it('should remove multiple gifs from the page if multiple exist', () => {
+
+    });
+
+    it('should still run even if there are no gifs on the page', () => {
+
+    });
+});
+    
